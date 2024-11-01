87229a9fdf98e71f0f0f9bbe9ba5d32758696b26
Shubham Darade <138992223+ShubhamDarade@users.noreply.github.com>
2024-11-02 00:16:56 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL });
 
-export const register = (data) => API.post("/auth/register", data);
-export const login = (data) => API.post("/auth/login", data);
+export const register = (data) => API.post("/api/auth/register", data);
+export const login = (data) => API.post("/api/auth/login", data);
